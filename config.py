3c3eb83becfefe88ebbf02170c5231fa0274c720
Set Rosa <setrosa199@gmail.com>
2026-05-04 11:12:04 +0700

    Add environment variables and configuration settings

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
 
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
+ABA_QR_URL = "payment_qr.jpg"
+WELCOME_IMAGE_URL = "image copy.png"
+CHANNEL_URL = os.getenv("CHANNEL_URL", "https://t.me/your_channel")

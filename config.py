9d6b2d0dcda2669004b04b874e97e3bceea5e306
Set Rosa <setrosa199@gmail.com>
2026-05-23 22:08:06 +0700

    Update welcome image URL in config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import os
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 ABA_QR_URL = "payment_qr.jpg"
-WELCOME_IMAGE_URL = "image copy.png"
+WELCOME_IMAGE_URL = "bot welcome.png"
 CHANNEL_URL = os.getenv("CHANNEL_URL", "https://t.me/your_channel")
 _admin_ids_env = os.getenv("ADMIN_IDS", "")
 if _admin_ids_env:
